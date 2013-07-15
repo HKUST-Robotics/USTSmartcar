a04c1ed914a86e2ba746a3a30038da0bd06b81c7
Louis <louismkk_0312@hotmail.com>
2013-07-16 01:30:58 +0800

    Backup before start tune speed 1500

--- src/app/isr.c
+++ src/app/isr.c
@@ -9,19 +9,25 @@ int ccd_compressed_print_flag=0;               // 0: off, 1: on
 int only_balance_pid_mode=0;                   // 0: off, 1: on
 
 /*********** startup PID values ************/
-int speed_array[3]              = {300    , 900    , 600};
-int balance_kp_array[3]         = {2414746, 2725000, 2700000};
-int balance_kd_array[3]         = {99160  , 119160 , 94160};
-int balance_offset_array[3]     = {1177   , 1177   , 1177};
-int speed_kp_array[3]           = {297000 , 297000 , 297000};
-int speed_ki_array[3]           = {49500  , 49500  , 49500};
-int turn_kp_array[3]            = {120500 , 91800  , 120500};
-int turn_kd_array[3]            = {100000 , 7500   , 100000}; // mode 1 is orginal 12475 (tested not work now)
-int turn_offset_array[3]        = {1230   , 1230   , 1230}; //·U²Ó·U¥k¡A·U¤j·U¥ª
-int left_start_length_array[3]  = {25     , 37     , 31}; // mode 0 to be edit to 45
-int right_start_length_array[3] = {25     , 37     , 31}; // mode 0 to be edit to 45
+int speed_array[5]              = {300    , 900    , 600};
+int balance_kp_array[5]         = {2414746, 2725000, 2700000};
+int balance_kd_array[5]         = {99160  , 119160 , 94160};
+int balance_offset_array[5]     = {1177   , 1177   , 1177};
+int speed_kp_array[5]           = {297000 , 297000 , 297000};
+int speed_ki_array[5]           = {49500  , 49500  , 49500};
+int turn_kp_array[5]            = {120500 , 91800  , 120500};
+int turn_kd_array[5]            = {100000 , 7500   , 100000}; // mode 1 is orginal 12475 (tested not work now)
+int turn_offset_array[5]        = {1230   , 1230   , 1230}; //·U²Ó·U¥k¡A·U¤j·U¥ª
+int left_start_length_array[5]  = {25     , 37     , 31}; // mode 0 to be edit to 45
+int right_start_length_array[5] = {25     , 37     , 31}; // mode 0 to be edit to 45
 int ccd_mid_pos_array[3]        = {128    , 128    , 128};
-int run_speed_mode = 2; // vaild input : 0 , 1 , 2
+int run_speed_mode = 0; // vaild input : 0 , 1 , 2 , 3 , 4
+/*** 
+mode 0: 300
+mode 1: 900
+mode 2: 600
+mode 3: 1500
+***
 
 /*********** initialize balance PID ************/
 int balance_kp = 0;
@@ -298,7 +304,7 @@ void pit3_system_loop(void){
             ----- */    
 
         if (gpio_get(PORTE, 8) == 0){ // when 3 press
-          run_speed_mode = 0;
+          run_speed_mode++;
         }
         else if (gpio_get(PORTE, 9) == 0){ //when 4 press
           run_speed_mode = 1;
